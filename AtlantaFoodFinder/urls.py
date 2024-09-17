d3504e0ad66518fc07318b880ef3d58f27d0cb1d
aidanmascoli <aidantm717@gmail.com>
2024-09-16 21:07:56 -0400

    add frontend templates (index, log in, sign up)

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -5,5 +5,12 @@ from . import views
 app_name = "atl_food_finder"
 
 urlpatterns = [
+    path('', views.index_view, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
     path('map/', views.map_view, name='map'),
+    path('settings/', views.construction_view, name='settings'),
+    path('reviews/', views.construction_view, name='reviews'),
+    path('reset/', views.construction_view, name='reset'),
+    path('recover/', views.construction_view, name='recover')
 ]
--- AtlantaFoodFinder/views.py
+++ AtlantaFoodFinder/views.py
@@ -1,8 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from .models import Restaurant
 from django.conf import settings
 
+
+def index_view(request):
+    return render(request, 'AtlantaFoodFinder/index.html')
+
+
+def login_view(request):
+    return render(request, 'AtlantaFoodFinder/login.html')
+
+
+def signup_view(request):
+    return render(request, 'AtlantaFoodFinder/signup.html')
+
+
 def map_view(request):
-    return render(request, 'map.html', {'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY})
+    return render(request, 'AtlantaFoodFinder/map.html', {'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY})
+
+
+def construction_view(request):
+    return HttpResponse('This page is under construction', status=503)
