61f5a5c704227a32fec6b75b5d525fa677a6114f
Daniel Mittelman <mittelmandaniel@gmail.com>
2024-10-01 14:30:01 -0400

    Added Profile Model

--- AtlantaFoodFinder/admin.py
+++ AtlantaFoodFinder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Profile, Restaurant, Favorite
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Restaurant)
+admin.site.register(Favorite)
--- AtlantaFoodFinder/models.py
+++ AtlantaFoodFinder/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -7,9 +8,7 @@ class Restaurant(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
 
-class Favorite(models.Model):
-    name = models.CharField(max_length=100)
-    location = (models.DecimalField(max_digits=8, decimal_places=6),
-                models.DecimalField(max_digits=8, decimal_places=6))
-    def __str__(self):
-        return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorites = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
