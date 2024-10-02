5e873d5890a5802c13b3de576851543637956ee2
Matthew Nehme-Haily <matthewnehme04@gmail.com>
2024-10-01 22:17:08 -0400

    Added email for signup

--- AtlantaFoodFinder/views.py
+++ AtlantaFoodFinder/views.py
@@ -1,12 +1,28 @@
+from django.contrib.auth import user_logged_in
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
+from django import forms
+from django.contrib.auth.models import User
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text='Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "AtlantaFoodFinder/signup.html"
 
